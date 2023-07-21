f3d2a1f8bae7e870d030c1bfa0fe8fd8da4f9366
IT21130052 <it21130052@my.sliit.lk>
2023-07-21 14:29:15 -0700

    code added
    code added

---
+++ booklist_project/booklist/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
---
+++ booklist_project/booklist/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('create/', views.book_create, name='book_create'),
+    path('update/<int:pk>/', views.book_update, name='book_update'),
+    path('delete/<int:pk>/', views.book_delete, name='book_delete'),
+    path('search/', views.book_search, name='book_search'),
+]
